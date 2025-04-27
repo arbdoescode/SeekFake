af435c004ba3c8727e2fc8622ea61a9d39e113fb
linditasuta <lindisuta@gmail.com>
2025-04-27 13:38:38 +0200

    task done

--- service/fetchapikeys.py
+++ service/fetchapikeys.py
@@ -1,5 +1,9 @@
 import asyncio 
+import hashlib
 
 #create method that, returns a random hashed id of (md5) format
 def Md5Format():
-    print("test")
+    result=hashlib.md5(b'BrokenRoad')
+    print(result.hexdigest())
+
+Md5Format()
