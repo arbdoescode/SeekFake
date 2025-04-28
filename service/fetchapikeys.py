574915132cf04d88136ebbe8ad0f0c15f9080c3a
Arb Koçi <arbbisha@gmail.com>
2025-04-28 09:55:57 +0200

    Setup requirements for api deployment

--- service/fetchapikeys.py
+++ service/fetchapikeys.py
@@ -1,9 +1,7 @@
 import asyncio 
 import hashlib
 
-#create method that, returns a random hashed id of (md5) format
 def Md5Format():
     result=hashlib.md5(b'BrokenRoad')
     print(result.hexdigest())
 
-Md5Format()
