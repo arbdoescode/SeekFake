94c117fbe6dcd2878889b7cde474e5f8d3dfbdde
Lindita Suta <lindisuta@gmail.com>
2025-05-20 13:48:50 +0200

    Login/Logout Methods

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from config.database import db
 from module.Request import UserAuthReq,ExampleReq
 from module.Response import UserRes
 from module.Request.Account import RegisterUserReq
+from module.Request.Account import LogInReq
 from module.Response.BaseRes import BaseResp
 from service import auth,aigenerating,ipsessions
 from starlette.requests import Request
@@ -16,7 +17,7 @@ from typing import List
 
 app = FastAPI()
 
-app.add_middleware(token_auth.CheckValueMiddleware, skip_paths=["/test/","/register/"])
+app.add_middleware(token_auth.CheckValueMiddleware, skip_paths=["/test/","/register/","/login/"])
 
 # Example methods
 
@@ -57,7 +58,27 @@ async def iplist_call(sess:ExampleReq.SimpleReq):
     return response_text
 
 # Lindita Suta controllers
-    
+   
+   
+#LOGIN/LogOut & REGISTER   
+ 
+@app.post("/register/")
+async def RegisterNewUser(item:RegisterUserReq.RegUserReq):
+    response_text = await auth.registerNewUser(item)
+    return response_text  
+
+@app.post("/login/")
+async def RegisterNewUser(item:LogInReq.LogInUsr):
+    response_text = await auth.logIn(item)
+    return response_text  
+
+@app.get("/logout/{token}")
+async def RegisterNewUser(token: str):
+    response_text = await auth.logout(token)
+    return response_text  
+
+#TESTING
+
 @app.get("/test-getusers/")
 async def test_get_users():
     users = await auth.fetch_users_Test()
@@ -68,12 +89,11 @@ async def RegisterNewUser(item:UserAuthReq.UserAuth):
     response_text = await auth.registerNewUser_Test(item)
     return response_text
 
-@app.post("/register/")
-async def RegisterNewUser(item:RegisterUserReq.RegUserReq):
-    response_text = await auth.registerNewUser(item)
+@app.post("/testnew-register/")
+async def RegisterNewUser(item:UserAuthReq.UserAuth):
+    response_text = await auth.register_user(item)
     return response_text
 
-
-
+#FINISH TESTING
 
 # endregion LS
