3d4b13d02d9fa09a55e0230841df1f351eedc50e
Arb Koçi <akoci19@epoka.edu.al>
2025-05-27 16:30:45 +0200

    Upgrade driver 17 to 18

--- build.sh
+++ build.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-# Update and install system dependencies
-apt-get update && \
-apt-get install -y curl gnupg2 gcc g++ libpq-dev unixodbc-dev
+set -e  # Stop the script if any command fails
 
-# Add Microsoft package repo and install the ODBC driver
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - && \
+# Install prerequisites
+apt-get update
+apt-get install -y curl gnupg2 apt-transport-https software-properties-common
+
+# Add Microsoft package signing key
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+
+# Add Microsoft SQL Server repository (for Debian 11/Ubuntu 20+)
 curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
-apt-get update && \
-ACCEPT_EULA=Y apt-get install -y msodbcsql17
+# Update and install ODBC Driver 18
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql18 unixodbc-dev gcc g++ libpq-dev
 
 # Upgrade pip and install Python dependencies
 pip install --upgrade pip
